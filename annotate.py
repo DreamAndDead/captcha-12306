a60d733cb17b45c5281e5338780ae9494e117ac3
DreamAndDead <favorofife@yeah.net>
2020-04-03 11:39:32 +0800

    annotate image script

--- annotate.py
+++ annotate.py
@@ -27,60 +27,80 @@ def ocr(img):
     r = requests.post(url, data=data, params=params, headers=headers)
     return r.json()
 
+def get_captcha_text_label(anno_text_dir):
+    serial_labels = {}
+    
+    for file_path in list(paths.list_images(anno_text_dir)):
+        basename = os.path.basename(file_path)
+        (serial, ext) = os.path.splitext(basename)
+        label = os.path.basename(os.path.dirname(file_path))
+
+        serial_labels[serial] = label
+
+    return serial_labels
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='annotate text/image raw data')
-    parser.add_argument('-t', '--type', type=str, required=True, help='data type')
+    parser.add_argument('-t', '--type', choices=['text', 'image'], required=True, help='data type')
     parser.add_argument('-r', '--raw', type=str, required=True, help='raw data directory to annotate')
-    parser.add_argument('-o', '--output', type=str, required=True, help='target directory to save ocr result')
+    parser.add_argument('-a', '--annotated_text', type=str, help='annotated text dir, only need when type is image')
+    parser.add_argument('-o', '--output', type=str, required=True, help='target directory to save annotated result')
     args = vars(parser.parse_args())
 
-    text_dir = args['text']
+    raw_type = args['type']
+    raw_dir = args['raw']
+    anno_text_dir = args['annotated_text']
     output_dir = args['output']
 
-    os.makedirs(output_dir, exist_ok=True)
+    if raw_type == 'image' and not anno_text_dir:
+        print("-a option is needed when type is image")
+        exit()
 
-    text_paths = list(paths.list_images(text_dir))
+    if raw_type == 'image':
+        serial_labels = get_captcha_text_label(anno_text_dir)
+    
+    raw_paths = list(paths.list_images(raw_dir))
     output_paths = list(paths.list_images(output_dir))
     output_files = list(map(os.path.basename, output_paths))
-    
-    for text_path in text_paths:
-        basename = os.path.basename(text_path)
+
+    for raw_path in raw_paths:
+        basename = os.path.basename(raw_path)
         if basename in output_files:
             print("%s is already annotated." % basename)
             continue
 
-        img = cv2.imread(text_path)
-        # https://cloud.baidu.com/doc/OCR/s/zk3h7xz52#%E8%AF%B7%E6%B1%82%E8%AF%B4%E6%98%8E
-        # width and height are 15px at least
-        (h, w, _) = img.shape
-        if h < 15:
-            size = (int(w * 15 / h), 15)
-            img = cv2.resize(img, size, interpolation=cv2.INTER_AREA)
+        raw_data = cv2.imread(raw_path)
 
-        res = ocr(img)
+        if raw_type == 'text':
+            # https://cloud.baidu.com/doc/OCR/s/zk3h7xz52#%E8%AF%B7%E6%B1%82%E8%AF%B4%E6%98%8E
+            # width and height are 15px at least
+            (h, w, _) = raw_data.shape
+            if h < 15:
+                size = (int(w * 15 / h), 15)
+                raw_data = cv2.resize(raw_data, size, interpolation=cv2.INTER_AREA)
 
-        if res.get('error_code', None):
-            # such as qps limit, etc.
-            print("some error returns from ocr api.")
-            break
-        elif res.get('words_result_num') == 0:
-            # can't recoganize
-            label = 'unknown'
-        else:
-            # most possible value
-            label = res['words_result'][0]['words']
+            res = ocr(raw_data)
+            time.sleep(0.5)
 
+            if res.get('error_code', None):
+                # such as qps limit, etc.
+                print("some error returns from ocr api.")
+                break
+            elif res.get('words_result_num') == 0:
+                # can't recoganize
+                label = 'unknown'
+            else:
+                # most possible value
+                label = res['words_result'][0]['words']
+        elif raw_type == 'image':
+            serial, _ = basename.split('_')
+            label = serial_labels[serial]
 
         print("recoganize %s as label %s" % (basename, label))
         
         output_path = os.path.join(output_dir, label, basename)
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-        shutil.copy(text_path, output_path)
-
-        time.sleep(0.5)
-
-        
+        shutil.copy(raw_path, output_path)
 
 
